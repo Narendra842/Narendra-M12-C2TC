d9d4967eea3d0eb9ede48be6ef4ee404b00b61cf
Narendra842 <joshinarendradivakar@gmail.com>
2022-03-16 14:49:28 +0530

    Hello world and Addition Program

---
+++ M12Day_first/src/com/m12/Addition.java
@@ -0,0 +1,16 @@
+package com.m12;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int firstNo=34;
+		int secondNo=45;
+		
+		int result = firstNo + secondNo;
+		
+		System.out.println("Addition is : "+result);
+
+	}
+
+}
